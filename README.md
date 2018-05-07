a4387f5a39437ba3336b12a994cae573997e25d1
Sebastian Dimunzio <sdimunzio@gmail.com>
2018-05-07 10:30:28 -0300

    RDITZ-2 Updating HTTP port number, and build configuration

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 RDI Tanzania Dashboard
+
---
+++ dashboard-api/src/main/java/org/devgateway/rdi/tanzania/controllers/Boundaries.java
@@ -0,0 +1,10 @@
+package org.devgateway.rdi.tanzania.controllers;
+
+/**
+ * @author Sebastian Dimunzio
+ */
+
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Boundaries {
+}
