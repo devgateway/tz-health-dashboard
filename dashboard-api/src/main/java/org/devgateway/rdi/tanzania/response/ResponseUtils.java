96634bbea1c5e98becfe4536afc5d85ef40f7605
Sebastian Dimunzio <sdimunzio@gmail.com>
2018-08-22 12:26:52 -0300

    end points updates

---
+++ dashboard-api/src/main/java/org/devgateway/rdi/tanzania/response/ResponseUtils.java
@@ -0,0 +1,32 @@
+package org.devgateway.rdi.tanzania.response;
+
+import org.devgateway.rdi.tanzania.domain.Facility;
+
+/**
+ * @author Sebastian Dimunzio
+ */
+
+public class ResponseUtils {
+
+
+    public static FacilityResponse facilityToResponse(Facility facility) {
+        FacilityResponse facilityResponse = new FacilityResponse();
+        facilityResponse.setId(facility.getId());
+        facilityResponse.setCode(facility.getCode());
+        facilityResponse.setName(facility.getName());
+
+        facilityResponse.setType(facility.getType());
+        facilityResponse.setDetailedType(facility.getDetailedType());
+
+        facilityResponse.setOwnership(facility.getOwnership());
+        facilityResponse.setDetailedOwnership(facility.getDetailedOwnership());
+
+        facilityResponse.setWard(new BoundaryResponse(facility.getWard().getGid(), facility.getWard().getName()));
+        facilityResponse.setDistrict(new BoundaryResponse(facility.getWard().getDistrict().getGid(), facility.getWard().getDistrict().getName()));
+        facilityResponse.setRegion(new BoundaryResponse(facility.getWard().getDistrict().getRegion().getGid(), facility.getWard().getDistrict().getRegion().getName()));
+        facilityResponse.setCoordinates(new Double[]{facility.getPoint().getCoordinate().x, facility.getPoint().getCoordinate().y});
+
+        return facilityResponse;
+
+    }
+}
