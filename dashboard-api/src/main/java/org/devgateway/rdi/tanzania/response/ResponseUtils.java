0711274bf0ea1957569c1c8df5e01d5d090c7d48
Sebastian Dimunzio <sdimunzio@gmail.com>
2018-08-22 15:20:21 -0300

    updating end points uris

--- dashboard-api/src/main/java/org/devgateway/rdi/tanzania/response/ResponseUtils.java
+++ dashboard-api/src/main/java/org/devgateway/rdi/tanzania/response/ResponseUtils.java
@@ -10,23 +10,26 @@ public class ResponseUtils {
 
 
     public static FacilityResponse facilityToResponse(Facility facility) {
-        FacilityResponse facilityResponse = new FacilityResponse();
-        facilityResponse.setId(facility.getId());
-        facilityResponse.setCode(facility.getCode());
-        facilityResponse.setName(facility.getName());
+        if (facility != null) {
+            FacilityResponse facilityResponse = new FacilityResponse();
+            facilityResponse.setId(facility.getId());
+            facilityResponse.setCode(facility.getCode());
+            facilityResponse.setName(facility.getName());
 
-        facilityResponse.setType(facility.getType());
-        facilityResponse.setDetailedType(facility.getDetailedType());
+            facilityResponse.setType(facility.getType());
+            facilityResponse.setDetailedType(facility.getDetailedType());
 
-        facilityResponse.setOwnership(facility.getOwnership());
-        facilityResponse.setDetailedOwnership(facility.getDetailedOwnership());
+            facilityResponse.setOwnership(facility.getOwnership());
+            facilityResponse.setDetailedOwnership(facility.getDetailedOwnership());
 
-        facilityResponse.setWard(new BoundaryResponse(facility.getWard().getGid(), facility.getWard().getName()));
-        facilityResponse.setDistrict(new BoundaryResponse(facility.getWard().getDistrict().getGid(), facility.getWard().getDistrict().getName()));
-        facilityResponse.setRegion(new BoundaryResponse(facility.getWard().getDistrict().getRegion().getGid(), facility.getWard().getDistrict().getRegion().getName()));
-        facilityResponse.setCoordinates(new Double[]{facility.getPoint().getCoordinate().x, facility.getPoint().getCoordinate().y});
+            facilityResponse.setWard(new BoundaryResponse(facility.getWard().getGid(), facility.getWard().getName()));
+            facilityResponse.setDistrict(new BoundaryResponse(facility.getWard().getDistrict().getGid(), facility.getWard().getDistrict().getName()));
+            facilityResponse.setRegion(new BoundaryResponse(facility.getWard().getDistrict().getRegion().getGid(), facility.getWard().getDistrict().getRegion().getName()));
+            facilityResponse.setCoordinates(new Double[]{facility.getPoint().getCoordinate().x, facility.getPoint().getCoordinate().y});
 
-        return facilityResponse;
-
+            return facilityResponse;
+        } else {
+            return null;
+        }
     }
 }
