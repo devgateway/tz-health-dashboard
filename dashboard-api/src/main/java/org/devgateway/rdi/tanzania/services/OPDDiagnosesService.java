27fec4de96050c9fd10322d540b8f764520119f8
Sebastian Dimunzio <sdimunzio@gmail.com>
2018-08-29 10:16:56 -0300

    Returning OPD by age

--- dashboard-api/src/main/java/org/devgateway/rdi/tanzania/services/OPDDiagnosesService.java
+++ dashboard-api/src/main/java/org/devgateway/rdi/tanzania/services/OPDDiagnosesService.java
@@ -2,10 +2,12 @@ package org.devgateway.rdi.tanzania.services;
 
 import org.devgateway.rdi.tanzania.domain.Facility;
 import org.devgateway.rdi.tanzania.repositories.OPDDiagnosticRepository;
+import org.devgateway.rdi.tanzania.response.OPDByAgeResponse;
 import org.devgateway.rdi.tanzania.response.OPDResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,11 +23,32 @@ public class OPDDiagnosesService {
     FacilityService facilityService;
 
 
-    public List<OPDResponse> getByYear(Long id, Integer year) {
+    public List<OPDByAgeResponse> getByYear(Long id, Integer year) {
 
         Facility f = facilityService.getFacility(id);
         List<OPDResponse> opdResponses = opdDiagnosticRepository.getAllYearly(f, year);
-        return opdResponses;
+
+        List<OPDByAgeResponse> results = new ArrayList<>();
+        Long current = null;
+        OPDByAgeResponse opdByAgeResponse = null;
+
+        for (OPDResponse opd : opdResponses) {
+
+            if (current != opd.getDiagnostic().getId()) {
+                current = opd.getDiagnostic().getId();
+                opdByAgeResponse = new OPDByAgeResponse();
+                opdByAgeResponse.setDiagnostic(opd.getDiagnostic());
+                opdByAgeResponse.setYear(opd.getYear());
+                results.add(opdByAgeResponse);
+            }
+
+            opdByAgeResponse.addValue(opd.getAge(), opd.getValue());
+
+
+        }
+
+
+        return results;
 
     }
 }
