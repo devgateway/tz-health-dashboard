490e7b27543c55fba9680b64efc61ff379a0edef
mbertoli <mbertoli@developmentgateway.org>
2018-08-30 10:12:00 -0300

    refactor

--- dashboard-ui/src/routes/dashboard/routes/facilityReport/containers/facilityContainer.js
+++ dashboard-ui/src/routes/dashboard/routes/facilityReport/containers/facilityContainer.js
@@ -1,18 +1,23 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
-import { getFacilityInfo } from '../modules/facilityModule'
+import { getFacilityInfo, getFacilityPopulation, getFacilityDiagnoses, getMapShape, getMapPoints } from '../modules/facilityModule'
 import Layout from '../layouts/facilityLayout'
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-	onGetFacilityInfo: getFacilityInfo
+	onGetFacilityInfo: getFacilityInfo,
+	onGetFacilityPopulation: getFacilityPopulation,
+	onGetFacilityDiagnoses: getFacilityDiagnoses,
+	onGetMapPoints: getMapPoints,
+	onGetMapShape: getMapShape,
 }, dispatch)
 
 const mapStateToProps = state => {
   return {
   	mapShape: state.getIn(['facility', 'reportData', 'map', 'shape']).toJS(),
   	mapPoints: state.getIn(['facility', 'reportData', 'map', 'points']).toJS(),
-  	mainInfo: state.getIn(['facility', 'reportData', 'main']).toJS(),
-  	populationInfo: state.getIn(['facility', 'reportData', 'population']).toJS(),
+  	info: state.getIn(['facility', 'reportData', 'info']).toJS(),
+  	population: state.getIn(['facility', 'reportData', 'population']).toJS(),
+  	diagnoses: state.getIn(['facility', 'reportData', 'diagnoses']).toJS(),
   }
 }
 
--- dashboard-ui/src/routes/dashboard/routes/facilityReport/modules/facilityModule.js
+++ dashboard-ui/src/routes/dashboard/routes/facilityReport/modules/facilityModule.js
@@ -5,37 +5,49 @@ import * as api from '../../../../../api'
 const FACILITY_INFO_REQUEST = 'FACILITY_INFO_REQUEST'
 const FACILITY_INFO_RESPONSE = 'FACILITY_INFO_RESPONSE'
 const FACILITY_INFO_ERROR = 'FACILITY_INFO_ERROR'
+const FACILITY_POPULATION_REQUEST = 'FACILITY_POPULATION_REQUEST'
+const FACILITY_POPULATION_RESPONSE = 'FACILITY_POPULATION_RESPONSE'
+const FACILITY_POPULATION_ERROR = 'FACILITY_POPULATION_ERROR'
+const FACILITY_DIAGNOSES_REQUEST = 'FACILITY_DIAGNOSES_REQUEST'
+const FACILITY_DIAGNOSES_RESPONSE = 'FACILITY_DIAGNOSES_RESPONSE'
+const FACILITY_DIAGNOSES_ERROR = 'FACILITY_DIAGNOSES_ERROR'
+
 const FACILITY_MAP_REQUEST = 'FACILITY_MAP_SHAPE_REQUEST'
 const FACILITY_MAP_SHAPE_RESPONSE = 'FACILITY_MAP_SHAPE_RESPONSE'
 const FACILITY_MAP_POINTS_RESPONSE = 'FACILITY_MAP_POINTS_RESPONSE'
+const FACILITY_MAP_ERROR = 'FACILITY_MAP_ERROR'
 
 // ------------------------------------ Actions ------------------------------------
 
-export const getFacilityInfo = (id, infoType) => {
-  if (infoType) {
-    return getFacilityInfoByType(id, infoType)
-  } else {
-    return (dispatch, getState) => {
-      dispatch({type: FACILITY_INFO_REQUEST})
-      api.getFacilityInfo(id).then(data => {
-        dispatch({'type': FACILITY_INFO_RESPONSE, data, infoType: 'main' })
-        dispatch({'type': FACILITY_MAP_REQUEST})
-        dispatch(getMapShape(data))
-        dispatch(getMapPoints(data))
-      }).catch(error => {
-        dispatch({'type': FACILITY_INFO_ERROR, error})
-      })
-    }
+export const getFacilityInfo = (id) => {
+  return (dispatch, getState) => {
+    dispatch({type: FACILITY_INFO_REQUEST})
+    api.getFacilityData(id).then(data => {
+      dispatch({'type': FACILITY_INFO_RESPONSE, data})
+    }).catch(error => {
+      dispatch({'type': FACILITY_INFO_ERROR, error})
+    })
   }
 };
 
-const getFacilityInfoByType = (id, infoType) => {
+export const getFacilityPopulation = (id) => {
   return (dispatch, getState) => {
-    dispatch({type: FACILITY_INFO_REQUEST})
-    api.getFacilityInfo(id, infoType).then(data => {
-      dispatch({'type': FACILITY_INFO_RESPONSE, data, infoType })
+    dispatch({type: FACILITY_POPULATION_REQUEST})
+    api.getFacilityData(id, 'population').then(data => {
+      dispatch({'type': FACILITY_POPULATION_RESPONSE, data})
     }).catch(error => {
-      dispatch({'type': FACILITY_INFO_ERROR, error})
+      dispatch({'type': FACILITY_POPULATION_ERROR, error})
+    })
+  }
+};
+
+export const getFacilityDiagnoses = (id) => {
+  return (dispatch, getState) => {
+    dispatch({type: FACILITY_DIAGNOSES_REQUEST})
+    api.getFacilityData(id, 'diagnoses').then(data => {
+      dispatch({'type': FACILITY_DIAGNOSES_RESPONSE, data})
+    }).catch(error => {
+      dispatch({'type': FACILITY_DIAGNOSES_ERROR, error})
     })
   }
 };
@@ -48,8 +60,11 @@ export const getMapShape = (facilityData) => {
     params = {regions: facilityData.region.id}
   }
   return (dispatch, getState) => {
+    dispatch({type: FACILITY_MAP_REQUEST})
     getShapeMethod(params).then(data => {
       dispatch({'type': FACILITY_MAP_SHAPE_RESPONSE, data })
+    }).catch(error => {
+      dispatch({'type': FACILITY_MAP_ERROR, error})
     })
   }
 };
@@ -60,8 +75,11 @@ export const getMapPoints = (facilityData) => {
     params = {regions: facilityData.region.id, type: facilityData.type.dhis2Id}
   }
   return (dispatch, getState) => {
+    dispatch({type: FACILITY_MAP_REQUEST})
     api.findFacilities(params).then(data => {
       dispatch({'type': FACILITY_MAP_POINTS_RESPONSE, data })
+    }).catch(error => {
+      dispatch({'type': FACILITY_MAP_ERROR, error})
     })
   }
 };
@@ -70,42 +88,97 @@ export const getMapPoints = (facilityData) => {
 // ------------------------------------ Action Handlers ------------------------------------
 const ACTION_HANDLERS = {
   [FACILITY_INFO_REQUEST]: (state, action) => {
-    return state.setIn(['reportData', 'loading'], true)
+    return state.setIn(['reportData', 'info', 'loading'], true)
   },
   [FACILITY_INFO_RESPONSE]: (state, action) => {
-    const {data, infoType} = action;
-    return state.setIn(['reportData', infoType], Immutable.fromJS(data)).setIn(['reportData', 'loading'], false)
-  },
-  [FACILITY_MAP_REQUEST]: (state, action) => {
-    return state.setIn(['reportData', 'map', 'shape'], Immutable.fromJS({})).setIn(['reportData', 'map', 'points'], Immutable.fromJS([]))
-  },
-  [FACILITY_MAP_SHAPE_RESPONSE]: (state, action) => {
     const {data} = action;
-    return state.setIn(['reportData', 'map', 'shape'], Immutable.fromJS(data))
-  },
-  [FACILITY_MAP_POINTS_RESPONSE]: (state, action) => {
-    const {data} = action;
-    return state.setIn(['reportData', 'map', 'points'], Immutable.fromJS(data))
+    return state.setIn(['reportData', 'info'], Immutable.fromJS(data)).setIn(['reportData', 'info', 'loading'], false)
   },
   [FACILITY_INFO_ERROR]: (state, action) => {
     const {error} = action;
-    return state.setIn(['reportData', 'error'], error).setIn(['reportData', 'loading'], false)
+    return state.setIn(['reportData', 'info', 'error'], error).setIn(['reportData', 'info', 'loading'], false)
   },
+
+  [FACILITY_POPULATION_REQUEST]: (state, action) => {
+    return state.setIn(['reportData', 'population', 'loading'], true)
+  },
+  [FACILITY_POPULATION_RESPONSE]: (state, action) => {
+    const {data} = action;
+    return state.setIn(['reportData', 'population', 'data'], Immutable.fromJS(getAggregatedPopulation(data))).setIn(['reportData', 'population', 'loading'], false)
+  },
+  [FACILITY_POPULATION_ERROR]: (state, action) => {
+    const {error} = action;
+    return state.setIn(['reportData', 'population', 'error'], error).setIn(['reportData', 'population', 'loading'], false)
+  },
+
+  [FACILITY_DIAGNOSES_REQUEST]: (state, action) => {
+    return state.setIn(['reportData', 'diagnoses', 'loading'], true)
+  },
+  [FACILITY_DIAGNOSES_RESPONSE]: (state, action) => {
+    const {data} = action;
+    return state.setIn(['reportData', 'diagnoses', 'data'], Immutable.fromJS(data)).setIn(['reportData', 'diagnoses', 'loading'], false)
+  },
+  [FACILITY_DIAGNOSES_ERROR]: (state, action) => {
+    const {error} = action;
+    return state.setIn(['reportData', 'diagnoses', 'error'], error).setIn(['reportData', 'diagnoses', 'loading'], false)
+  },
+
+  [FACILITY_MAP_REQUEST]: (state, action) => {
+    return state.setIn(['reportData', 'map', 'loading'], true)
+  },
+  [FACILITY_MAP_SHAPE_RESPONSE]: (state, action) => {
+    const {data} = action;
+    return state.setIn(['reportData', 'map', 'shape'], Immutable.fromJS(data)).setIn(['reportData', 'map', 'loading'], false)
+  },
+  [FACILITY_MAP_POINTS_RESPONSE]: (state, action) => {
+    const {data} = action;
+    return state.setIn(['reportData', 'map', 'points'], Immutable.fromJS(data)).setIn(['reportData', 'map', 'loading'], false)
+  },
+  [FACILITY_MAP_ERROR]: (state, action) => {
+    const {error} = action;
+    return state.setIn(['reportData', 'map', 'error'], error).setIn(['reportData', 'map', 'loading'], false)
+  },
+  
 };
+
+// ------------------------------------ Helpers ------------------------------------
+
+const getAggregatedPopulation = (populationInfo) => {
+  const total = sumPopulationValue(populationInfo)
+  const totalMale = sumPopulationValue(populationInfo.filter(i => i.gender.name === 'ME'))
+  const totalFemale = sumPopulationValue(populationInfo.filter(i => i.gender.name === 'KE'))
+  const totalUnder5 = sumPopulationValue(populationInfo.filter(i => i.age.name === '< 1' || i.age.name === '1-4'))
+  const total5to60 = sumPopulationValue(populationInfo.filter(i => i.age.name !== '< 1' && i.age.name !== '1-4' && i.age.name !== '60+'))
+  const totalAbove60 = sumPopulationValue(populationInfo.filter(i => i.age.name === '60+'))   
+  return {total, totalMale, totalFemale, totalUnder5, total5to60, totalAbove60}
+}
+
+const sumPopulationValue = (filteredDataset) => {
+  let total = 0
+  filteredDataset.forEach(i => total += i.value)
+  return total
+}
 
 // ------------------------------------ Reducer ------------------------------------
 const initialState = Immutable.fromJS({
   'reportData': {
-    'loading': false, 
-    'main': {
-      type: {},
-      ward: {},
-      district: {},
-      region: {},
+    'info': {
+      'loading': false,
+      'type': {},
+      'ward': {},
+      'district': {},
+      'region': {},
     },
-    population: [],
-    'error': null,
+    'population': {
+      'loading': false,
+      'data': {},
+    },
+    'diagnoses': {
+      'loading': false,
+      'data': [],
+    },
     'map': {
+      'loading': false,
       'shape': {},
       'points': [],
     }
