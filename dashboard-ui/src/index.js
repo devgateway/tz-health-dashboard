586b8a0e665c79a17625fc60a8eb4133ca9f0844
Sebastian Dimunzio <sdimunzio@gmail.com>
2018-08-31 15:35:17 -0300

    translation library

--- dashboard-ui/src/index.js
+++ dashboard-ui/src/index.js
@@ -4,5 +4,11 @@ import AppContainer from './appContainer'
 import './index.css'
 import store, {history} from './store/store'
 const routes = require('./routes/index').default(store);
+import { I18nextProvider } from "react-i18next";
 
-render(<AppContainer routes={routes} store={store} history={history}></AppContainer>, document.querySelector('#root'))
+import i18n from "./i18n";
+
+render(
+  <I18nextProvider i18n={i18n}>
+   <AppContainer routes={routes} store={store} history={history}></AppContainer>
+  </I18nextProvider>, document.querySelector('#root'))
