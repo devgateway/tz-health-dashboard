dc5bf975a07a9a48ded6aa172dd474fc18fccd2c
Sebastian Dimunzio <sdimunzio@gmail.com>
2018-08-31 15:35:17 -0300

    translation library

---
+++ dashboard-ui/src/components/lanSelector.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import {translate, Trans} from "react-i18next";
+
+class Selector extends Component {
+  render() {
+    const { t, i18n } = this.props;
+
+    const changeLanguage = lng => {
+      debugger;
+      i18n.changeLanguage(lng);
+    };
+
+    return (<div>
+        <button onClick={() => changeLanguage("de")}>de</button>
+      <button onClick={() => changeLanguage("en")}>en</button>
+    </div>);
+  }
+}
+
+// extended main view with translate hoc
+export default translate("translations")(Selector);
--- dashboard-ui/src/layouts/defaultLayout.js
+++ dashboard-ui/src/layouts/defaultLayout.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Selector from '../components/lanSelector'
 
 
 export default (props) =>(
   <div className="app">
+        <div><Selector></Selector></div>
         {props.children}
   </div>)
