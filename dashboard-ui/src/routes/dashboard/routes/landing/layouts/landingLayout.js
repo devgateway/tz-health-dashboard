6b70c9d71cd07a25d41687cfe29e45bbbfc58a06
mbertoli <mbertoli@developmentgateway.org>
2018-09-17 17:55:55 -0300

    landing page

--- dashboard-ui/src/routes/dashboard/routes/landing/layouts/landingLayout.js
+++ dashboard-ui/src/routes/dashboard/routes/landing/layouts/landingLayout.js
@@ -1,14 +1,40 @@
 import React from 'react'
-//import Map from '../components/landingMaps.jsx'
-import {translate, Trans} from "react-i18next";
-import {Link} from 'react-router-dom'
+import {translate, Trans} from "react-i18next"
+import PropTypes from "prop-types"
 
-const component= (props) =>(
-  <div>
-    <h2><Trans>Landing Page</Trans></h2>
-    <div><Link to='/generate/facility'>Facility Report Generator</Link></div>
-    <div><Link to='/generate/ward'>Ward Report Generator</Link></div>
-  </div>
-)
+class LandingLayout extends React.Component {
 
-export default translate("translations")(component)
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return(
+  	  <div className="landing-page">
+  	    <div className="landing-title">Data Report Generator</div>
+  	    <div className="landing-paragrph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut blandit dui. Vivamus a ligula imperdiet, mollis orci sit amet, viverra quam. Etiam laoreet cursus lacus et eleifend. In hac habitasse platea dictumst. Proin elit risus, varius non accumsan sed, malesuada ut erat. Mauris mollis odio eget dui placerat vehicula.</div>
+  	   	<div className="report-options">
+  		    <div onClick={e => this.context.router.history.push('/generate/facility')}>
+            <div className="facility-icon"></div>
+            <div className="option-title">Facility Report Generator</div>
+          </div>
+  		    <div onClick={e => this.context.router.history.push('/generate/ward')}>
+            <div className="ward-icon"></div>
+            <div className="option-title">Ward Report Generator</div>
+          </div>
+  		  </div>
+        <div className="info-box">
+          <div className="info-icon"></div>
+          <div className="info-paragraph">Donec a ultrices mi, quis viverra est. Duis nec nisl justo. Cras maximus nibh at volutpat pretium. Proin rutrum egestas diam vitae sodales. Vestibulum rhoncus a lectus in tempus. </div>
+        </div>
+  	  </div>
+  	)
+  }
+}
+
+export default translate("translations")(LandingLayout)
