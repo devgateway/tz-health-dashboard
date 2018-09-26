fb14ff747387e0ca4e7c277b26a0a4adc68123cd
Sebastian Dimunzio <sdimunzio@gmail.com>
2018-09-26 09:34:29 -0300

    scripts update

--- bin/metadata.sh
+++ bin/metadata.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Xmx512m -Xms1024m -cp dashboard-api-1.0-SNAPSHOT.jar -Dloader.main=org.devgateway.rdi.tanzania.Dhis2MetadataImport org.springframework.boot.loader.PropertiesLauncher
+java -Xmx512m -Xms1024m -cp dashboard-api-1.0-SNAPSHOT.jar -DLOG_DIR=. -Dloader.main=org.devgateway.rdi.tanzania.Dhis2MetadataImport org.springframework.boot.loader.PropertiesLauncher
