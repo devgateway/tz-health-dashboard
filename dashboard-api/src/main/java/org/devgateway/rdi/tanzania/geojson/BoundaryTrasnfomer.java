c4f36c89a4c7f96d4d1890fcd6bb3d942dcb935c
Sebastian Dimunzio <sdimunzio@gmail.com>
2018-11-06 12:35:59 -0300

    RDITZ-133

--- dashboard-api/src/main/java/org/devgateway/rdi/tanzania/geojson/BoundaryTrasnfomer.java
+++ dashboard-api/src/main/java/org/devgateway/rdi/tanzania/geojson/BoundaryTrasnfomer.java
@@ -1,8 +1,8 @@
 package org.devgateway.rdi.tanzania.geojson;
 
-import com.bedatadriven.jackson.datatype.jts.GeoJson;
 import org.devgateway.geo.GeoJsonUtils;
 import org.devgateway.rdi.tanzania.domain.Boundary;
+import org.devgateway.rdi.tanzania.domain.Ward;
 import org.geojson.Feature;
 
 /**
@@ -16,6 +16,24 @@ public class BoundaryTrasnfomer {
         Feature f = new Feature();
         f.setProperty("ID", boundary.getGid());
         f.setProperty("NAME", boundary.getName());
+        f.setGeometry(GeoJsonUtils.jtsGeometryToGeoJson(boundary.getGeom()));
+        return f;
+    }
+
+
+    public static Feature transform(Ward boundary, boolean includePopulation) {
+        Feature f = new Feature();
+        f.setProperty("ID", boundary.getGid());
+        f.setProperty("NAME", boundary.getName());
+        if (includePopulation) {
+            f.setProperty("POPULAION", boundary.getPopulation());
+            f.setProperty("POPULAION_FEMALE", boundary.getPopulationFemale());
+            f.setProperty("POPULAION_MALE", boundary.getPopulationMale());
+            f.setProperty("POPULAION_RURAL", boundary.getPopulationRural());
+            f.setProperty("POPULAION_RURAL_FEMALE", boundary.getPopulationRuralFemale());
+            f.setProperty("POPULAION_RURAL_MALE", boundary.getPopulationRuralFemale());
+
+        }
         f.setGeometry(GeoJsonUtils.jtsGeometryToGeoJson(boundary.getGeom()));
         return f;
     }
