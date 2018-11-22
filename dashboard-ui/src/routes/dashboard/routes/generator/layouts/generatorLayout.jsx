90903831a07949806fd9898f97d9eb694e67cfd8
mbertoli <mbertoli@developmentgateway.org>
2018-11-22 11:22:28 -0300

    report generator changes

--- dashboard-ui/src/routes/dashboard/routes/generator/layouts/generatorLayout.jsx
+++ dashboard-ui/src/routes/dashboard/routes/generator/layouts/generatorLayout.jsx
@@ -72,7 +72,7 @@ class WardLayout extends React.Component {
     if (reportType === 'facility') {
       selectFacility(selection.id)
     } else {
-      selectWard(selection.id)
+      selectWard(selection.gid)
     }    
   }
 
@@ -143,6 +143,13 @@ class WardLayout extends React.Component {
     selectFacility(feature.properties.ID)
   }
 
+  onReset() {
+    const { selectRegion } = this.props
+    this.child.onCleanSelection() //clear keyword search box
+    selectRegion(null) //clear location dropdowns selections
+    this.setState({period: null})//clear the period selector
+  }
+
   onGenerateReport() {
     const lan = this.props.i18n.language
     const { period } = this.state
@@ -176,7 +183,9 @@ class WardLayout extends React.Component {
     }
     if (ward.get('selected')) {
       const wardFeature = ward.get('list').toJS().features.find(f => f.properties.ID == ward.get('selected'))
-      mapShapes = {'type': 'FeatureCollection', 'features': [wardFeature]}
+      if (wardFeature) {
+        mapShapes = {'type': 'FeatureCollection', 'features': [wardFeature]}
+      }
     } else if (district.get('selected')) {
       mapShapes = ward.get('list').toJS()
     } else if (region.get('selected')){
@@ -198,108 +207,93 @@ class WardLayout extends React.Component {
         mapPoints = {'type': 'FeatureCollection', 'features': facilitiesFeatures}
       }
     }
-    debugger
-  	return (
+    
+    return (
   	  <div className="report-generator-container">
   	    <div className="">
           <div className="report-generator-paragraph">
+            <div className={`${reportType}-icon`}></div>
             <span className={`highlighted-${reportType}`}><Trans>{`${reportType} report generator`}</Trans>: </span>
             {reportType === 'ward' ?
-              <span className="">
-                <Trans>Create a custom data report for a ward by </Trans>
-                1.<b><Trans>Selecting a Ward </Trans></b>
-                <Trans>by typing its name in the </Trans>
-                <i><Trans>Ward Search </Trans></i>
-                <Trans>or filtering by location using the </Trans>
-                <i><Trans>Ward Filter </Trans></i>
-                <Trans>and map, and then </Trans>
-                2.<b><Trans>Selecting a Time Period </Trans></b>
-                <Trans>in the </Trans>
-                <i><Trans>Date Filter section </Trans></i>
-              </span>
+              <span className=""><Trans>Create a custom data report for a ward</Trans></span>
             :
-              <span className="">
-                <Trans>Create a custom data report for a health facility by </Trans>
-                1.<b><Trans>Selecting a Facility </Trans></b>
-                <Trans>by typing its name in the </Trans>
-                <i><Trans>Facility Search </Trans></i>
-                <Trans>or filtering by location using the </Trans>
-                <i><Trans>Facility Filter </Trans></i>
-                <Trans>and map, and then </Trans>
-                2.<b><Trans>Selecting a Time Period </Trans></b>
-                <Trans>in the </Trans>
-                <i><Trans>Date Filter section </Trans></i>
-              </span>
+              <span className=""><Trans>Create a custom data report for a health facility</Trans></span>
             }
           </div>
           <div className="dashed-separator"></div>
-          <div className=""><TextSearch searchType={reportType} onSelection={e => this.onKeywordSearchSelection(e)} onRef={ref => (this.child = ref)}/></div>
-          <div className="dashed-separator"></div>
-          <div className="generator-by-path">
-            <div className="generator-dropdowns">
-              <div className={`report-type-${reportType}`}>
-                <Trans>{`${reportType} filter`}</Trans>
-              </div>
-              <div className="path-dropdown">
-                <div className="dropdown-title"><Trans>Regions</Trans></div>
-                <div className="">
-                  <select value={region.get('selected') || -1} className="" onChange={e => this.onChangeRegion(e)} >
-                    <option value={-1}><Trans>Select a region</Trans></option>
-                    {region.getIn(['list', 'features']).map(option => {
-                      return <option key={`region-${option.getIn(['properties', 'ID'])}`} value={option.getIn(['properties', 'ID'])}>{option.getIn(['properties', 'NAME'])}</option>
-                    })}
-                  </select>
+          <div className="step-one-box">
+            <div className="step-descriptor"><div><Trans>Step one</Trans>: </div><b><Trans>Select a Facility</Trans></b> <Trans>by typing it name in the</Trans> <i><Trans>Facility Search</Trans> </i>below</div>
+            <TextSearch searchType={reportType} onSelection={e => this.onKeywordSearchSelection(e)} onRef={ref => (this.child = ref)}/>
+            <div className="step-descriptor"><div><Trans>OR</Trans>: </div><b><Trans>Select a Facility</Trans></b> <Trans>by location using the</Trans> <i><Trans>Facility filter</Trans></i> box or map below</div>
+            <div className="generator-by-path">
+              <div className="generator-dropdowns">
+                <div className={`report-type-${reportType}`}>
+                  <Trans>{`${reportType} filter`}</Trans>
                 </div>
-              </div>
-              <div className="path-dropdown">
-                <div className="dropdown-title"><Trans>Districts</Trans></div>
-                <div className="">
-                  <select value={district.get('selected') || -1} className="" onChange={e => this.onChangeDistrict(e)} >
-                    <option value={-1}><Trans>Select a district</Trans></option>
-                    {district.getIn(['list', 'features']).map(option => {
-                      return <option key={`district-${option.getIn(['properties', 'ID'])}`} value={option.getIn(['properties', 'ID'])}>{option.getIn(['properties', 'NAME'])}</option>
-                    })}
-                  </select>
-                </div>
-              </div>
-              <div className="path-dropdown">
-                <div className="dropdown-title"><Trans>Wards</Trans></div>
-                <div className="">
-                  <select value={ward.get('selected') || -1} className="" onChange={e => this.onChangeWard(e)} >
-                    <option value={-1}><Trans>Select a ward</Trans></option>
-                    {ward.getIn(['list', 'features']).map(option => {
-                      return <option key={`ward-${option.getIn(['properties', 'ID'])}`} value={option.getIn(['properties', 'ID'])}>{option.getIn(['properties', 'NAME'])}</option>
-                    })}
-                  </select>
-                </div>
-              </div>
-              {(reportType === 'facility') ?
                 <div className="path-dropdown">
-                  <div className="dropdown-title"><Trans>Facilities</Trans></div>
+                  <div className="dropdown-title"><Trans>Regions</Trans></div>
                   <div className="">
-                    <select value={facility.get('selected') || -1} className="" onChange={e => this.onChangeFacility(e)} >
-                      <option value={-1}><Trans>Select a facility</Trans></option>
-                      {facility.getIn(['list']).map(option => {
-                        return <option key={`facility-${option.getIn(['id'])}`} value={option.getIn(['id'])}>{option.getIn(['name'])}</option>
+                    <select value={region.get('selected') || -1} className="" onChange={e => this.onChangeRegion(e)} >
+                      <option value={-1}><Trans>Select a region</Trans></option>
+                      {region.getIn(['list', 'features']).map(option => {
+                        return <option key={`region-${option.getIn(['properties', 'ID'])}`} value={option.getIn(['properties', 'ID'])}>{option.getIn(['properties', 'NAME'])}</option>
                       })}
                     </select>
                   </div>
                 </div>
-              : null}        
+                <div className="path-dropdown">
+                  <div className="dropdown-title"><Trans>Districts</Trans></div>
+                  <div className="">
+                    <select value={district.get('selected') || -1} className="" onChange={e => this.onChangeDistrict(e)} >
+                      <option value={-1}><Trans>Select a district</Trans></option>
+                      {district.getIn(['list', 'features']).map(option => {
+                        return <option key={`district-${option.getIn(['properties', 'ID'])}`} value={option.getIn(['properties', 'ID'])}>{option.getIn(['properties', 'NAME'])}</option>
+                      })}
+                    </select>
+                  </div>
+                </div>
+                <div className="path-dropdown">
+                  <div className="dropdown-title"><Trans>Wards</Trans></div>
+                  <div className="">
+                    <select value={ward.get('selected') || -1} className="" onChange={e => this.onChangeWard(e)} >
+                      <option value={-1}><Trans>Select a ward</Trans></option>
+                      {ward.getIn(['list', 'features']).map(option => {
+                        return <option key={`ward-${option.getIn(['properties', 'ID'])}`} value={option.getIn(['properties', 'ID'])}>{option.getIn(['properties', 'NAME'])}</option>
+                      })}
+                    </select>
+                  </div>
+                </div>
+                {(reportType === 'facility') ?
+                  <div className="path-dropdown">
+                    <div className="dropdown-title"><Trans>Facilities</Trans></div>
+                    <div className="">
+                      <select value={facility.get('selected') || -1} className="" onChange={e => this.onChangeFacility(e)} >
+                        <option value={-1}><Trans>Select a facility</Trans></option>
+                        {facility.getIn(['list']).map(option => {
+                          return <option key={`facility-${option.getIn(['id'])}`} value={option.getIn(['id'])}>{option.getIn(['name'])}</option>
+                        })}
+                      </select>
+                    </div>
+                  </div>
+                : null}        
+              </div>
+              <div className="generator-map">
+                {mapShapes.features.length > 0 ?
+                  <D3Map width="490" height="480" colors={["#FF8C42", '#0C4700']}
+                    shapeFillOpacity="0" shapeStrokeWidth='2' shapeStrokeColor="#9C8568"
+                    onFeatureClick={f => this.onFeatureClick(f)} onPointClick={f => this.onFacilityClicked(f)}
+                    shapeFeatures={mapShapes} pointFeatures={mapPoints} showBasemap={true}  zoomeable={true}></D3Map>
+                : null}
+              </div>
             </div>
-            <div className="generator-map">
-              {mapShapes.features.length > 0 ?
-                <D3Map width="500" height="480" colors={["#FF8C42", '#0C4700']}
-                  shapeFillOpacity="0" shapeStrokeWidth='2' shapeStrokeColor="#9C8568"
-                  onFeatureClick={f => this.onFeatureClick(f)} onPointClick={f => this.onFacilityClicked(f)}
-                  shapeFeatures={mapShapes} pointFeatures={mapPoints} showBasemap={true}  zoomeable={true}></D3Map>
-              : null}
-            </div>
-            <div className={(reportType === 'facility' && facility.get('selected')) || (reportType === 'ward' && ward.get('selected')) ? 'generator-period' : 'generator-period disabled'}>
-              <div className="period-header"><Trans>Date Filter</Trans></div>
-              <div className="period-paragraph"><Trans>To complete the report generator it is required to select a Timeframe and Period. Once selected the Generate Report button will engage to complete the report generator process. Use the blue button to clear all search and filter content.</Trans></div>
+          </div>
+          <div className={(reportType === 'facility' && facility.get('selected')) || (reportType === 'ward' && ward.get('selected')) ? 'step-two-box' : 'step-two-box disabled'}>
+            <div className="step-descriptor"><div><Trans>Step Two</Trans>: </div><b><Trans>Select a Timeframe</Trans></b></div>
+            <div className="generator-period">
+              <div className={`period-header-${reportType}`}><Trans>Date Filter</Trans></div>
+              <div className="period-paragraph"><Trans>After selecting a {reportType} select the time period (yearly, quarterly, or monthly) and desired dates</Trans></div>
               <div className="path-dropdown">
-                <div className="dropdown-title"><Trans>Timeframe</Trans></div>
+                <div className="dropdown-title"><Trans>Time Period</Trans></div>
                 <div className="">
                   <select value={periodType} className="" onChange={e => this.onChangeType(e)}>
                     <option value="monthly">{i18n.t('Monthly')}</option>
@@ -309,9 +303,9 @@ class WardLayout extends React.Component {
                 </div>
               </div>
               <div className="path-dropdown">
-                <div className="dropdown-title"><Trans>Period</Trans></div>
+                <div className="dropdown-title"><Trans>Date</Trans></div>
                 <div className="">
-                  <select onChange={e => this.onChangePeriod(e)} value={period}>
+                  <select onChange={e => this.onChangePeriod(e)} value={period || -1}>
                     <option value={-1}>{i18n.t('Select a period')}</option>
                     {
                       options.map(option => {
@@ -321,8 +315,11 @@ class WardLayout extends React.Component {
                   </select>
                 </div>
               </div>
+              <div className="period-paragraph">
+                <Trans>Click on the</Trans> <div className="generate"><Trans>Generate Report</Trans></div> <Trans>button to create your custom data report, or click on the</Trans> <div className="reset"><Trans>Reset</Trans></div> <Trans>button to clear all search and filter selections</Trans>                 
+              </div>
               <div className="action-buttons">
-                <div className="reset-button"><Trans>R</Trans></div>
+                <div className="reset-button" onClick={e => this.onReset()}></div>
                 {period ?
                   <div className="generate-button" onClick={e => this.onGenerateReport()}><Trans>Generate Report</Trans></div>
                 :
@@ -333,7 +330,9 @@ class WardLayout extends React.Component {
           </div>
           <div className="info-box">
             <div className="info-icon"></div>
-            <div className="info-paragraph"><Trans>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis facilisis placerat. In convallis ante enim, nec pulvinar nibh cursus nec. Donec quis vulputate eros.</Trans></div>
+            <div className="info-paragraph">
+              <Trans>The</Trans> <div className="generate"><Trans>Generate Report</Trans></div> <Trans>will not engage until you have selected a valid {reportType}, time period, and date. First, select a {reportType}, either through the search or filter boxes in Step One, then select a time period in Step Two.</Trans>
+            </div>
           </div>
         </div>
   	  </div>
