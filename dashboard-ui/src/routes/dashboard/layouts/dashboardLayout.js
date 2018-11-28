b757edf35e38c978a0afc14027b3c2592025eb6f
mbertoli <mbertoli@developmentgateway.org>
2018-11-28 15:44:31 -0300

    new share buttons

--- dashboard-ui/src/routes/dashboard/layouts/dashboardLayout.js
+++ dashboard-ui/src/routes/dashboard/layouts/dashboardLayout.js
@@ -4,11 +4,9 @@ import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {push} from 'react-router-redux'
 import {translate, Trans} from "react-i18next";
-import Share from './share.jsx'
 const component = (props) => {
   return (
     <div className="dashboard">
-      <div><Share></Share></div>
       {props.children}
     </div>)
 }
