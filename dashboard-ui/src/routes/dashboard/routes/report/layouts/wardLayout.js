14b5e7dbc26cb0ef69293e3303e3816316df6611
mbertoli <mbertoli@developmentgateway.org>
2018-11-29 16:38:21 -0300

    copy share button

--- dashboard-ui/src/routes/dashboard/routes/report/layouts/wardLayout.js
+++ dashboard-ui/src/routes/dashboard/routes/report/layouts/wardLayout.js
@@ -7,6 +7,8 @@ import PeriodSelector from '../components/periodSelector'
 import Legends from '../components/legends'
 import {print} from '../utils/printUtil'
 import {translate, Trans} from "react-i18next"
+import CopyShare from '../components/copyShareLink'
+import i18n from '../../../../../i18n'
 
 class WardLayout extends React.Component {
 
@@ -98,7 +100,8 @@ class WardLayout extends React.Component {
       <div>
         <div className="report-header">
           <div className="ward-name">{wardName} <Trans>Ward</Trans></div>
-          <div className="print-icon" onClick={e => this.printReport()}></div>
+          <div title={`${i18n.t('Print as PDF')}`} className="print-icon" onClick={e => this.printReport()}></div>
+          <CopyShare/>
           <PeriodSelector period={period} params={this.props.params} onChangePeriod={e => this.onChangePeriod(e)}/>
         </div>
         <div className="ward-report-container">
