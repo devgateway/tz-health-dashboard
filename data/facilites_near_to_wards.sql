c07c109051f33b3a8a2f532376eae26e25ab8d4e
Sebastian Dimunzio <sdimunzio@gmail.com>
2018-12-10 16:28:17 -0300

    updating facilities with nearest ward when ward was not found

--- data/facilites_near_to_wards.sql
+++ data/facilites_near_to_wards.sql
@@ -1,3 +1,19 @@
+CREATE OR REPLACE FUNCTION public.get_nearest_ward_by_facility(bigint)
+  RETURNS record AS
+$BODY$
+DECLARE
+	ret record;
+BEGIN
+raise notice 'Value: %', $1;
+
+select ST_Distance(a.point,b.geom) ,a.id, a.name,b.gid,b.name into ret from  facility a , boundary b where  id=$1 and b.dtype='WARD' order by ST_Distance(a.point,b.geom)  asc limit 1;
+RETURN ret;
+END;
+$BODY$
+ LANGUAGE plpgsql;
+
+
+
 update facility as f set ward_gid=t.ward_id
 from
 	facility f1 ,
