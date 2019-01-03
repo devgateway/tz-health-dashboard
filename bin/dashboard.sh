1c6c1cb7e26bb9fc70e77e8462d84426884f3f69
Sebastian Dimunzio <sdimunzio@gmail.com>
2019-01-03 15:56:09 -0300

    scripting refactoring

--- bin/dashboard.sh
+++ bin/dashboard.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Xms256m  -Xmx2048m -cp dashboard-api-1.0-SNAPSHOT.jar -DLOG_DIR=./logs  "$@"
+java -Xms256m  -Xmx2048m -cp dashboard-api-1.0-SNAPSHOT.jar org.devgateway.rdi.tanzania.Application -DLOG_DIR=./logs  "$@"
