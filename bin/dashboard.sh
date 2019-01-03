93c8cc395329df362032989692d146e627125568
Sebastian Dimunzio <sdimunzio@gmail.com>
2019-01-03 16:01:10 -0300

    scripting refactoring

--- bin/dashboard.sh
+++ bin/dashboard.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Xms256m  -Xmx2048m -cp dashboard-api-1.0-SNAPSHOT.jar org.devgateway.rdi.tanzania.Application -DLOG_DIR=./logs  "$@"
+java server -Xms1G -Xmx2G -XX:MaxMetaspaceSize=256m -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=128m -DLOG_DIR=/dashboard-ligs -DJava.awt.headless=true -XX:+CMSParallelRemarkEnabled -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -Xverify:none -noverify -jar dashboard-api-1.0-SNAPSHOT.jar --spring.config.name=staging
