670ba171640240ccf21f3b7983cb43265b108716
Sebastian Dimunzio <sdimunzio@gmail.com>
2019-01-04 16:42:44 -0300

    changes triggered during documentation

--- bin/dashboard.sh
+++ bin/dashboard.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-java -server -Xms1G -Xmx2G -XX:MaxMetaspaceSize=256m -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=128m -DLOG_DIR=./ -DLOG_FILE=dashboard.log  -DJava.awt.headless=true -XX:+CMSParallelRemarkEnabled -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -Xverify:none -noverify -jar dashboard-api-1.0-SNAPSHOT.jar --spring.config.name=staging
+java -server -Xms1G -Xmx2G -XX:MaxMetaspaceSize=256m -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=128m -DLOG_DIR=./ -DLOG_FILE=dashboard.log  -DJava.awt.headless=true -XX:+CMSParallelRemarkEnabled -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -Xverify:none -noverify -jar dashboard-api-1.0-SNAPSHOT.jar
+
+  ## Add the following argument to switch configuration file --spring.config.name=staging
