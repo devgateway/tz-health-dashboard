ea35c9a495bb5c26a6e0bdc2acc686fef219e2e3
Sebastian Dimunzio <sdimunzio@gmail.com>
2019-01-04 10:35:34 -0300

    scripting refactoring

--- bin/dashboard.sh
+++ bin/dashboard.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java server -Xms1G -Xmx2G -XX:MaxMetaspaceSize=256m -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=128m -DLOG_DIR=/dashboard-ligs -DJava.awt.headless=true -XX:+CMSParallelRemarkEnabled -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -Xverify:none -noverify -jar dashboard-api-1.0-SNAPSHOT.jar --spring.config.name=staging
+java -server -Xms1G -Xmx2G -XX:MaxMetaspaceSize=256m -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=128m -DLOG_DIR=/dashboard-ligs -DJava.awt.headless=true -XX:+CMSParallelRemarkEnabled -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -Xverify:none -noverify -jar dashboard-api-1.0-SNAPSHOT.jar --spring.config.name=staging
