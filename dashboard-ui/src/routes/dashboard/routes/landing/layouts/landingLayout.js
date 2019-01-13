195098ffde7e73dbac08daa32150de9bc7a4cb1d
galina kalvatcheva <gkalvatcheva@developmentgateway.org>
2019-01-13 18:23:09 -0500

    RDITZ-198
    - homepage styling improvements

--- dashboard-ui/src/routes/dashboard/routes/landing/layouts/landingLayout.js
+++ dashboard-ui/src/routes/dashboard/routes/landing/layouts/landingLayout.js
@@ -22,18 +22,19 @@ class LandingLayout extends React.Component {
     const lan= this.props.i18n.language
 
     return(
-  	  <div className="landing-page">
+  	  <div class="landing-page-wrapper">
+      <div className="landing-page">
   	    <div className="landing-title"><Trans>Data Report Generator</Trans></div>
   	    <div className="landing-paragrph"><Trans>This dashboard visualizes key information from Tanzania’s health information management system, dhis2. You can explore and download subnational data on service populations; out-patient diseases; reproductive, maternal, newborn, and child health; health financing; staffing; and community deaths. Use the buttons below to dive deeper into the data, by either an individual facility or ward.</Trans></div>
   	   	<div className="report-options">
-  		    <div onClick={e => this.context.router.history.push(`${lan}/generate/facility`)}>
+  		    <div onClick={e => this.context.router.history.push(`${lan}/generate/facility`)} className="report-box first">
             <div className="facility-icon"></div>
-            <div>
+            <div className="">
               <div className="option-title"><Trans>Facility Report Generator</Trans></div>
               <div className="option-paragraph"><Trans>Search and filter facility-level data to generate a monthly, quarterly, or annual health data report.</Trans></div>
             </div>
           </div>
-          <div onClick={e => this.context.router.history.push(`${lan}/generate/ward`)}>
+          <div onClick={e => this.context.router.history.push(`${lan}/generate/ward`)} className="report-box">
             <div className="ward-icon"></div>
             <div>
               <div className="option-title"><Trans>Ward Report Generator</Trans></div>
@@ -46,6 +47,7 @@ class LandingLayout extends React.Component {
           <div className="info-paragraph"><Trans>This portal is managed by the President’s Office - Regional Administration and Local Government (PORALG), in partnership with the Ministry of Health, Community Development, Gender, Elderly and Children (MoHCDGEC). Development Gateway developed the dashboard, with financial support from the Bill & Melinda Gates Foundation and technical support from DataVision International.</Trans></div>
         </div>
   	  </div>
+      </div>
   	)
   }
 }
